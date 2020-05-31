2c293e3d4cd8c82a5deda8630f19a8fe0893f53b
Luiyi Valentín <luiyivalentin18@gmail.com>
2020-05-31 10:42:55 -0400

    All authentication routes handlers completed.

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,12 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Current User');
+router.get('/api/users/currentuser', currentUser, requireAuth, (req: Request, res: Response) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,12 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
-router.post('/api/users/signout', (req, res) => {
-  res.send('Current User');
+router.post('/api/users/signout', (req: Request, res: Response) => {
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutRouter };
