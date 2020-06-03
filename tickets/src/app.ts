d9981f998dd15b4193387058be19b88755831613
Luiyi Valentín <luiyivalentin18@gmail.com>
2020-06-02 20:33:54 -0400

    Added MONGO_URI env variable

---
+++ tickets/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@tickets.dev/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+
+// Routes
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+// Middlewares
+app.use(errorHandler);
+
+export { app };
